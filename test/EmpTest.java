d1b78ae962d9bed39fdbf3c9d09e9ab82f7b87fa
vaishnavi <vaishnavipb2000@gmail.com>
2022-08-23 17:35:00 +0530

    Updated

--- test/EmpTest.java
+++ test/EmpTest.java
@@ -2,18 +2,30 @@ package com.sonata.test;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
+import com.sonata.Model.Employee;
+import com.sonata.impl.EmpImpl;
 
 
 public class EmpTest {
+	@Test
 	
-	public class EmpTest {
-@Test
 	   public void EmpTest() {
 		   Employee e3 = new Employee();
 		   EmpImpl e4 = new EmpImpl();
 		   e3.setEmpSal(100);
-		   Assertions.assertEquals(1500, e4.yearlySal(e3));
+		   Assertions.assertEquals(1200, e4.yearlySal(e3));
 		   
 	   }
+@Test
+public void EmpTest1() {
+	   Employee e3 = new Employee();
+	   EmpImpl e4 = new EmpImpl();
+	   e3.setEmpSal(100);
+	   Assertions.assertEquals(1000, e4.yearlySal(e3));
+	   	
+	
+	
 	}
+	
+	
 }
