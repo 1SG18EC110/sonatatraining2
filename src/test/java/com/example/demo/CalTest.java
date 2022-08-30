6846abfb1f8c5072ab600d82b53543577f278eb0
vaishnavi <vaishnavipb2000@gmail.com>
2022-08-30 14:15:38 +0530

    update

--- src/test/java/com/example/demo/CalTest.java
+++ src/test/java/com/example/demo/CalTest.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
 
 import org.junit.jupiter.api.Assertions;
 
---
+++ src/test/java/com/example/demo/Product.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class Product {
+	int pId;
+	String pName;
+	double pPrice;
+	
+	public Product(int pId, String pName, double pPrice) {
+		this.pId = pId;
+		this.pName = pName;
+		this.pPrice = pPrice;
+	}
+
+}
